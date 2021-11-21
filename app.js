008534f3b1cdaaf06bd101e6b715da52bf2ce3a4
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2021-11-21 01:22:57 -0600

    reparo http

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ io.on("connection", (socket) => {
   });
 });
 
-app.listen(app.get('port'), () => {
+server & app.listen(app.get('port'), () => {
   console.log(`Servidor en el puerto ${app.get('port')}`);
 });
